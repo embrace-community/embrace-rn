51859cf15273fbdfb8e9698557c5a01a7d2327a2
Martin Creedy <martin@openskydevelopment.co.uk>
2023-06-05 18:23:47 +0100

    Ethers v5 installed

--- App.tsx
+++ App.tsx
@@ -1,24 +1,22 @@
 import { Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
-import {
-  NativeStackScreenProps,
-  createNativeStackNavigator,
-} from '@react-navigation/native-stack';
+// Imports for ethers
+import 'react-native-get-random-values';
+import '@ethersproject/shims';
+import { ethers } from 'ethers';
+// Imports for react-navigation
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+// Example
 const isSetup = true;
-
-type RootStackParamList = {
-  Home: undefined;
-  Landing: undefined;
-};
 
 function HomeScreen({ navigation, route }) {
   return (
     <View className="flex-1 items-center justify-center bg-white p-4">
       <Text className="text-2xl font-bold text-center text-gray-800">
-        Open up App.tsx to start working on your app!
+        Open up App.tsx to start working on your app
       </Text>
       <TouchableOpacity
         onPress={() => {
@@ -50,11 +48,22 @@ function LandingScreen({ navigation }) {
 }
 
 function SettingsScreen() {
+  const createWallet = () => {
+    const wallet = ethers.Wallet.createRandom();
+    console.log('address:', wallet.address);
+    console.log('mnemonic:', wallet.mnemonic.phrase);
+    console.log('privateKey:', wallet.privateKey);
+  };
+
   return (
     <View className="flex-1 items-center justify-center bg-white p-4">
       <Text className="text-2xl font-bold text-center text-gray-800">
         Settings
       </Text>
+      <TouchableOpacity onPress={createWallet}>
+        <Text className="text-black">Create Wallet!</Text>
+        <Text className="hidden">Check console for output</Text>
+      </TouchableOpacity>
     </View>
   );
 }
